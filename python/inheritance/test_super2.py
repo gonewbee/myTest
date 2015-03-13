ab67cfc9b2bc0f5c92f995229bde69122308d3d8
zhangshy <zaozh1990@163.com>
2015-03-13 18:15:27 +0000

    测试python中super的使用

---
+++ python/inheritance/test_super2.py
@@ -0,0 +1,23 @@
+'''
+http://stackoverflow.com/questions/5033903/python-super-method-and-calling-alternatives
+'''
+class A(object):
+    def __init__(self):
+        print('Running A.__init__')
+        super(A,self).__init__()
+class B(A):
+    def __init__(self):
+        print('Running B.__init__')        
+        super(B,self).__init__()
+        #A.__init__(self) 
+
+class C(A):
+    def __init__(self):
+        print('Running C.__init__')
+        super(C,self).__init__()
+class D(B,C):
+    def __init__(self):
+        print('Running D.__init__')
+        super(D,self).__init__()
+
+foo=D()

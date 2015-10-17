20fd88f80211e09e4075d1078d9a0414a70533d0
ZhangShy <zaozh1990@163.com>
2015-10-17 13:30:14 +0800

    golang使用WebSocket
    js传递鼠标坐标，go根据坐标回复颜色值，在canvas中绘图

--- html5/WebSocket/wsdemo.go
+++ html5/WebSocket/wsdemo.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"golang.org/x/net/websocket"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+type mousePos struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
 
 func wsHandler(ws *websocket.Conn) {
 	var message string
-	websocket.Message.Receive(ws, &message)
-	log.Println("receive:" + message)
+	for nil == websocket.Message.Receive(ws, &message) {
+		log.Println("receive:" + message)
+		var pos mousePos
+		if err := json.Unmarshal([]byte(message), &pos); err != nil {
+			log.Panic("Unmarshal error!")
+			break
+		}
+		// 根据坐标回复颜色值
+		message = "#" + strconv.FormatInt(int64(pos.X*10), 16) + strconv.FormatInt(int64(pos.Y*10), 16)
+		websocket.Message.Send(ws, message)
+	}
+	log.Println("wsHandler end!!")
 }
 
 func main() {

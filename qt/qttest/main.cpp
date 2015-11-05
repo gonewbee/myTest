dbc560cb96219213936773f9e913dce7a32b921b
ZhangShy <zaozh1990@163.com>
2015-11-05 14:02:30 +0800

    qt添加图片资源

--- qt/qttest/main.cpp
+++ qt/qttest/main.cpp
@@ -1,12 +1,16 @@
 #include "mainwindow.h"
 #include "logindialog.h"
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 #if true
+    QIcon icon = QIcon(":/img/test.png");
+    qDebug() << icon.isNull();
+    a.setWindowIcon(icon);
     LoginDialog loginDialog;
     if (loginDialog.exec()==LoginDialog::Accepted) {
         w.show();

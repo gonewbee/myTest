98176ec7910c503740a907cb3a8ac9accccfc703
ZhangShy <zaozh1990@163.com>
2016-02-26 13:32:19 +0800

    python使用修饰符用来判断参数类型

---
+++ python/decorator/test_args.py
@@ -0,0 +1,23 @@
+"""使用修饰符decorator判断参数类型"""
+
+
+def check_arg(func):
+    def wrapper(l, *args):
+        if not isinstance(l, list):
+            raise Exception("l must be list")
+        print(func.__name__ + " was called!")
+        func(l, *args)
+    return wrapper
+
+
+@check_arg
+def test_func(l, n):
+    t = [i + n for i in l]
+    print(t)
+
+if __name__ == '__main__':
+    n = 10
+    l = list(range(10))
+    test_func(l, n)
+    l = 10
+    test_func(l, n)

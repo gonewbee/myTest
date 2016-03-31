e8be6cbba88cbe7b444eabcc03015e92eb3dc1c7
ZhangShy <zaozh1990@163.com>
2016-03-31 15:11:10 +0800

    添加OpenGL ES测试应用注释

--- OpenGLES/ES2/GLES_Render/gles2_triangle.c
+++ OpenGLES/ES2/GLES_Render/gles2_triangle.c
@@ -21,9 +21,11 @@ GLuint LoadShader ( GLenum type, const char *shaderSrc )
     return 0;
 
    // Load the shader source
+   // 加载shader源码
    glShaderSource ( shader, 1, &shaderSrc, NULL );
    
    // Compile the shader
+   // 编译shader
    glCompileShader ( shader );
 
    // Check the compile status
@@ -47,25 +49,30 @@ int triangle_init() {
       "   gl_Position = vPosition;  \n"
       "}                            \n";
    
+   // 颜色
    GLbyte fShaderStr[] =  
       "precision mediump float;\n"\
       "void main()                                  \n"
       "{                                            \n"
-      "  gl_FragColor = vec4 ( 1.0, 0.0, 0.0, 1.0 );\n"
+      "  gl_FragColor = vec4 ( 0.0, 1.0, 0.0, 1.0 );\n"
       "}                                            \n";
 
     GLuint vertexShader;
     GLuint fragmentShader;
     GLint linked;
     // Load the vertex/fragment shaders
+    // Vertex Shader 形状的顶点
     vertexShader = LoadShader ( GL_VERTEX_SHADER, vShaderStr );
+    // Fragment Shader 形状的颜色或纹理
     fragmentShader = LoadShader ( GL_FRAGMENT_SHADER, fShaderStr );
 
+    // Program 是OpenGL ES的对象，该对象包含用来绘制一个或多个形状的着色器(shader)
     // Create the program object
     programObject = glCreateProgram ( );
     if ( programObject == 0 ) {
         return 0;
     }
+    // 将顶点着色器添加到program对象
     glAttachShader ( programObject, vertexShader );
     glAttachShader ( programObject, fragmentShader );
 
@@ -73,6 +80,7 @@ int triangle_init() {
     glBindAttribLocation ( programObject, 0, "vPosition" );
 
     // Link the program
+    // 创建OpenGL ES的可执行程序
     glLinkProgram ( programObject );
 
     // Check the link status
@@ -92,9 +100,14 @@ int resize(int width, int height) {
 }
 
 int triangle_draw() {
-   GLfloat vVertices[] = {  0.0f,  0.5f, 0.0f, 
-                           -0.5f, -0.5f, 0.0f,
-                            0.5f, -0.5f, 0.0f };
+   GLfloat vVertices[] = {  // in counterclockwise order 逆时针顺序
+       0.0f,  0.5f, 0.0f,   // top
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       0.5f, -0.5f, 0.0f    // bottom right
+   };
+   // 计算顶点数目
+   int vertexCount = sizeof(vVertices)/(sizeof(GLfloat)*3);
+   // fprintf(stdout, "%s vertexCount:%d\n", __func__, vertexCount);
       
    // Set the viewport
    glViewport ( 0, 0, g_width, g_height );
@@ -109,5 +122,5 @@ int triangle_draw() {
    glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vVertices );
    glEnableVertexAttribArray ( 0 );
 
-   glDrawArrays ( GL_TRIANGLES, 0, 3 );
+   glDrawArrays ( GL_TRIANGLES, 0, vertexCount );
 }
